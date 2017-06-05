c0f5662328eb350123c9d1e9b1b041a1d0bcc6ae
bratislav94 <bratislav94@gmail.com>
2017-06-05 19:14:15 +0200

    Accommodation class

---
+++ BookingApp/BookingApp/BookingApp/BookingApp/Models/Accommodation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApp.Models
+{
+    public class Accommodation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public double AvrageGrade { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Approved { get; set; }
+
+        [ForeignKey("AccommodationType")]
+        public int AccommodationTypeId { get; set; }
+        public AccommodationType AccommodationType { get; set; }
+
+        public IList<Room> Rooms { get; set; }
+        public IList<Comment> Comments { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Place")]
+        public int PlaceId { get; set; }
+        public Place Place { get; set; }
+    }
+}
