8e85062ce5cd7bd154d415fd61ed2e56f9e69629
markoRkc94 <mrkstjkvc4@gmail.com>
2017-06-05 19:24:54 +0200

    Add AccommodationType

---
+++ BookingApp/BookingApp/BookingApp/BookingApp/Models/AccommodationType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingApp.Models
+{
+    public class AccommodationType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Accommodation> Accommodations { get; set; }
+    }
+}
