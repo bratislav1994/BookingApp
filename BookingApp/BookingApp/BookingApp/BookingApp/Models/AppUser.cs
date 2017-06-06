6e815becfbe45007894524af5a7763547a3b6109
bratislav94 <bratislav94@gmail.com>
2017-06-06 11:02:19 +0200

    AppUser class change

--- BookingApp/BookingApp/BookingApp/BookingApp/Models/AppUser.cs
+++ BookingApp/BookingApp/BookingApp/BookingApp/Models/AppUser.cs
@@ -8,6 +8,12 @@ namespace BookingApp.Models
     public class AppUser
     {
         public int Id { get; set; }
-        public int FullName { get; set; }
+        //public int FullName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public IList<RoomReservation> RoomReservations { get; set; }
+        public IList<Comment> Comments { get; set; }
+        public IList<Accommodation> Accommodations { get; set; }
     }
 }
