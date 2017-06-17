3b6b7f7a063960084b7674dc16f26ff386a2fed5
bratislav94 <bratislav94@gmail.com>
2017-06-17 15:12:10 +0200

    login modal dialog

--- BookingAppClient/src/app/app.component.ts
+++ BookingAppClient/src/app/app.component.ts
@@ -3,18 +3,22 @@ import { Router } from '@angular/router';
 import { DynamicUrl } from "app/DynamicUrl.model";
 import { LocalEnum } from "app/localEnum.model";
 import { LocalStorageService } from "app/local-storage.service";
+import { UserloginService } from "app/login/userlogin.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [LocalStorageService]
+  providers: [LocalStorageService, UserloginService]
 })
 export class AppComponent {
   title = 'app';
   username: string;
+  Username: string = "";
+  Password: string = "";
 
-  constructor(private storageService : LocalStorageService, private router: Router) { }
+  constructor(private storageService : LocalStorageService, private router: Router,
+              private userService: UserloginService) { }
 
   ngOnInit() {
      // this.router.navigate(['/home/']);
@@ -36,6 +40,22 @@ export class AppComponent {
 
   isUser() : boolean {
     return this.storageService.isUser();
+  }
+
+  logIn() {
+    this.userService.login(this.Username, this.Password, "password").subscribe(result => { 
+          localStorage.setItem(LocalEnum.User.toString(), result.json()['access_token']); // token
+          localStorage.setItem(LocalEnum.Role.toString(), result.headers.get("Role")); // Admin, Manager, AppUser
+          localStorage.setItem(LocalEnum.Id.toString(), result.headers.get("Id")); // user_id u accommodation
+          localStorage.setItem(LocalEnum.Username.toString(), this.Username); // estetski, gornji desni ugao :D
+          document.getElementById("close").click();
+          this.router.navigate(['/home']);
+        },
+    error => 
+     {
+        console.log(error), alert("Unsuccessful login")
+     }
+    );
   }
 
 }
